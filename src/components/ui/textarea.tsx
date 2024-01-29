6d8ac326ac672f779c3d3bce6ed09346113e92de
kano <ajvls98@gmail.com>
2024-01-29 17:02:30 +0900

    textarea box, 추가

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "nflex nmin-h-[80px] nw-full nrounded-md nborder nborder-input nbg-background npx-3 npy-2 ntext-sm nring-offset-background placeholder:ntext-muted-foreground focus-visible:noutline-none focus-visible:nring-2 focus-visible:nring-ring focus-visible:nring-offset-2 disabled:ncursor-not-allowed disabled:nopacity-50 resize-none",
+          "nflex nmin-h-[80px] nw-full nrounded-md nborder nborder-input nbg-background npx-3 npy-2 ntext-sm nring-offset-background placeholder:ntext-muted-foreground focus-visible:noutline-none focus-visible:nring-2 focus-visible:nring-ring focus-visible:nring-offset-2 disabled:ncursor-not-allowed disabled:nopacity-50 nresize-none",
           className
         )}
         ref={ref}
