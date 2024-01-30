d6f5d8598dcdb82bc4f2ba42a66cfd024ab45b6b
bogbu <glutamine082442@gmail.com>
2024-01-30 15:35:32 +0900

    Error page 생성

---
+++ src/templates/error/index.tsx
@@ -0,0 +1,12 @@
+import {ErrorResponse, useRouteError} from "react-router-dom";
+
+export default function ErrorPage() {
+    const error = useRouteError() as ErrorResponse;
+    return (
+        <div id="error-page">
+            <h1>Oops!</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <p>{error.status } {error.statusText}</p>
+        </div>
+    );
+}
