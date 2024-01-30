f9a5c5ddf7d4d6d4bc3907ce3a06bde7b8176107
bogbu <glutamine082442@gmail.com>
2024-01-30 18:19:30 +0900

    mock galleryList 생성

---
+++ src/mocks/galleryList.ts
@@ -0,0 +1,29 @@
+import {CardMockType} from "@/components/gallery/types.ts";
+
+export const mockCardList: CardMockType[] = [
+    {
+        img: "https://cdn.gameple.co.kr/news/photo/202308/206852_216550_1437.jpg",
+        title: "게시글1",
+        id: "1"
+    },
+    {
+        img: "https://cdn.gameple.co.kr/news/photo/202308/206852_216550_1437.jpg",
+        title: "게시글2",
+        id: "2"
+    },
+    {
+        img: "https://cdn.gameple.co.kr/news/photo/202308/206852_216550_1437.jpg",
+        title: "게시글3",
+        id: "3"
+    },
+    {
+        img: "https://cdn.gameple.co.kr/news/photo/202308/206852_216550_1437.jpg",
+        title: "게시글4",
+        id: "4"
+    },
+    {
+        img: "https://cdn.gameple.co.kr/news/photo/202308/206852_216550_1437.jpg",
+        title: "게시글5",
+        id: "5"
+    },
+];
