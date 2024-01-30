b6792c2bffda2649d9fd83d0c2822496c1f7918f
bogbu <glutamine082442@gmail.com>
2024-01-30 18:19:57 +0900

    갤러리 페이지에서 gallery Card mapping

--- src/templates/gallery/index.tsx
+++ src/templates/gallery/index.tsx
@@ -1,8 +1,22 @@
-const Gallery = () => {
+import "./gallery.scss";
+import Card from "@/components/gallery/Card.tsx";
+import {mockCardList} from "@/mocks/galleryList.ts";
+
+const Gallery = ({}) => {
+    //TODO 해당 부분 컨테이너로 부를 시 목 리스트를 상위에서 불러 하위로 전달하기
     return (
-        <>
-            Gallery
-        </>
+        <section className={"gallery"}>
+            <h1 className={"gallery__title"}>갤러리</h1>
+            <article className={"gallery__card--grid"}>
+                {
+                    mockCardList.map((item) => {
+                        return (
+                            <Card key={item.id} {...item}/>
+                        )
+                    })
+                }
+            </article>
+        </section>
     )
 }
 
