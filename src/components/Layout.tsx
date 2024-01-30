9140f11e20d08b8a039024eb3466dc9933024cd6
bogbu <glutamine082442@gmail.com>
2024-01-30 15:36:20 +0900

    기존 페이지 react node children props > outlet 으로 변경

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,20 +1,19 @@
-import React from "react";
 import Header from "@/components/Header.tsx";
 import Footer from "@/components/Footer.tsx";
+import {Outlet} from "react-router-dom";
 
 interface LayoutPropsType {
-    children: React.ReactNode,
     isHeader? : boolean,
     isFooter? : boolean
 }
-const Layout = ({children, isHeader = true, isFooter =  true} :LayoutPropsType) => {
+const Layout = ({isHeader = true, isFooter =  true} :LayoutPropsType) => {
     return (
         <section className={`layout nw-full`}>
             {
                 isHeader ? <Header/> : <></>
             }
             <section className={"contents nmax-w-screen-xl nm-auto"}>
-                {children}
+                <Outlet/>
             </section>
             {
                 isFooter ? <Footer/> : <></>
