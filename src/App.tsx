a711d6df612c80e970a7c781810211dbe3ece149
chloeun <chloekje22@gmail.com>
2024-02-02 00:09:05 +0900

    App.tsx 원상복귀

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ const router = createBrowserRouter([
             {
                 path: "absenceRequest",
                 // action: loginAction,
-                Component: AbsenceRequest,
+                Component: Main,
             },
             {
                 path: "absenceRequestDetails",
