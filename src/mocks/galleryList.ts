52d25dec8927ff850e0a10ae54e3176bfab335f9
bogbu <glutamine082442@gmail.com>
2024-02-01 12:02:11 +0900

    Edit Mock galleryList

--- src/mocks/galleryList.ts
+++ src/mocks/galleryList.ts
@@ -1,29 +1,34 @@
-import {CardMockType} from "@/components/gallery/types.ts";
+import {CardDetailMockType} from "@/components/gallery/types.ts";
 
-export const mockCardList: CardMockType[] = [
+export const mockCardList: CardDetailMockType[] = [
     {
         img: "https://cdn.gameple.co.kr/news/photo/202308/206852_216550_1437.jpg",
         title: "게시글1",
-        id: "1"
+        id: "1",
+        content : "게시글1"
     },
     {
         img: "https://cdn.gameple.co.kr/news/photo/202308/206852_216550_1437.jpg",
         title: "게시글2",
-        id: "2"
+        id: "2",
+        content : "게시글2"
     },
     {
         img: "https://cdn.gameple.co.kr/news/photo/202308/206852_216550_1437.jpg",
         title: "게시글3",
-        id: "3"
+        id: "3",
+        content : "게시글3"
     },
     {
         img: "https://cdn.gameple.co.kr/news/photo/202308/206852_216550_1437.jpg",
         title: "게시글4",
-        id: "4"
+        id: "4",
+        content : "게시글4"
     },
     {
         img: "https://cdn.gameple.co.kr/news/photo/202308/206852_216550_1437.jpg",
         title: "게시글5",
-        id: "5"
+        id: "5",
+        content : "게시글5"
     },
 ];
