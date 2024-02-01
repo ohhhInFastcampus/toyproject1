70b70c40b4f0e0d2d297dc54b7cf303122a0bf1b
bogbu <glutamine082442@gmail.com>
2024-02-01 23:08:55 +0900

    임시저장

--- src/containers/gallery/index.tsx
+++ src/containers/gallery/index.tsx
@@ -1,29 +1,24 @@
 import Gallery from "@/templates/gallery";
 import { db } from '@/Firebase.ts';
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDocs ,collection } from 'firebase/firestore';
 import {mockCardList} from "@/mocks/galleryList.ts";
+
 import {useEffect, useState} from "react";
 const GalleryPage = () => {
-    const [test, setTest] = useState()
-    async function getTest() {
-        // document에 대한 참조 생성
-        const docRef = doc(db, "gallery");
-        // 참조에 대한 Snapshot 쿼리
-        const docSnap = await getDoc(docRef);
-        console.log(docSnap,"docSnap")
-        if (docSnap.exists()) {
-
-            // setTest(docSnap.data())
-        }
-    }
+    const fruitCollection = collection(db, 'gallery')
+    const [fruits, setFruits] = useState([])
     useEffect(() => {
-        getTest()
+        async function getFruits() {
+            const data = await getDocs(fruitCollection)
+            setFruits(data.docs);
+        }
+        getFruits()
     }, [])
     return (
         // <Gallery list={mockCardList} />
         <div>
-            {test !== undefined &&
-                <div>{test.name}</div>}
+            {/*{test !== undefined &&*/}
+            {/*    <div>{test.name}</div>}*/}
         </div>
     )
 }
