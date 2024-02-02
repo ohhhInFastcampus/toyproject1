55b146f4dd664bf6408875db96931f9da0f6f852
Hong Joo Choi <hchoi2894@gmail.com>
2024-02-02 16:12:50 +0900

    type에러 수정

--- src/containers/main/index.tsx
+++ src/containers/main/index.tsx
@@ -1,10 +1,13 @@
 import Main from "@/templates/main";
+import { memberDetails } from "@/mocks/teamList";
 import { MemberDetailType } from "@/components/main/types";
 
-const MainPage = (props: MemberDetailType) => {
+const MainPage = () => {
+  const userData: MemberDetailType = memberDetails[0];
+  const userList: MemberDetailType[] = memberDetails;
   return (
     <div className="nmt-[40px] nmb-[40px]">
-      <Main {...props} />;
+      <Main user={userData} working={userList} />;
     </div>
   );
 };
--- src/templates/main/index.tsx
+++ src/templates/main/index.tsx
@@ -3,20 +3,23 @@ import { Clock } from "@/components/main/Clock";
 import { Calendar } from "@/components/main/Calendar";
 import { TodoList } from "@/components/main/TodoList";
 import WorkingStatus from "@/components/main/WorkingStatus";
-import { memberDetails } from "@/mocks/teamList";
 import { MemberDetailType } from "@/components/main/types";
 
-const Main = (props: MemberDetailType) => {
+interface MainPageType {
+  user: MemberDetailType;
+  working: MemberDetailType[];
+}
+const Main = ({ user, working }: MainPageType) => {
   return (
     <div className="nflex ngap-[40px]">
-      <UserMain {...props} />
+      <UserMain {...user} />
       <div className="nw-[400px] nm-5 nflex nflex-col nitems-center ngap-y-10">
         <Clock />
         <Calendar />
         <TodoList />
       </div>
       <div className="nw-[400px] nmt-5">
-        <WorkingStatus members={memberDetails} />
+        <WorkingStatus members={working} />
       </div>
     </div>
   );
