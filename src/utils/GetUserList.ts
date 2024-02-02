e0c2702054b28c5021ffd6317408ea05f7104f5a
bogbu <glutamine082442@gmail.com>
2024-02-02 18:39:09 +0900

    getUserList 메소드 생성

--- src/utils/GetUserList.ts
+++ src/utils/GetUserList.ts
@@ -1,15 +1,18 @@
-import {collection, doc, getDocs} from "firebase/firestore";
+import {collection, getDocs} from "firebase/firestore";
 import {db} from "@/Firebase.ts";
+import {MemberDetailType, MemberDetailTypes} from "@/components/main/types.ts";
 
 export const getUserList = async () => {
-    // const list =
+    const list: MemberDetailTypes[] = [];
     const userCollection = collection(db, "user");
     const userList = await getDocs(userCollection);
-    const docs = userList.forEach((item) => {
-
-        let newItem = {
-
-        }
-        console.log(item.id, item.data(), "item")
-    })
+    userList.forEach((item) => {
+            let obj : MemberDetailTypes = {
+                ...item.data() as MemberDetailType , email : item.id
+            }
+        list.push(obj);
+        return;
+    });
+    return list;
 }
+
