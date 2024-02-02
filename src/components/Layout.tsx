be7201334d4df5a570be0acfddee18fc42ab1703
bogbu <glutamine082442@gmail.com>
2024-02-02 14:42:11 +0900

    헤더에 ReactNode props 추가

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,19 +1,21 @@
 import Header from "@/components/Header.tsx";
 import Footer from "@/components/Footer.tsx";
-import {Outlet} from "react-router-dom";
+import {ReactNode} from "react";
 
 interface LayoutPropsType {
-    isHeader? : boolean,
-    isFooter? : boolean
+    isHeader?: boolean,
+    isFooter?: boolean,
+    children: ReactNode
 }
-const Layout = ({isHeader = true, isFooter =  true} :LayoutPropsType) => {
+
+const Layout = ({isHeader = true, isFooter = true, children}: LayoutPropsType) => {
     return (
         <section className={`layout nw-full`}>
             {
                 isHeader ? <Header/> : <></>
             }
             <section className={"contents nmax-w-screen-xl nm-auto"}>
-                <Outlet/>
+                {children}
             </section>
             {
                 isFooter ? <Footer/> : <></>
