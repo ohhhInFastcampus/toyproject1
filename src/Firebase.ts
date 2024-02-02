a5d5e8f35e4c822fc911614916b7d74cbc345595
bogbu <glutamine082442@gmail.com>
2024-02-02 13:54:59 +0900

    env 파일 삭제

--- src/Firebase.ts
+++ src/Firebase.ts
@@ -1,14 +1,13 @@
 import {initializeApp} from "firebase/app";
 import {getFirestore} from "firebase/firestore"
-
 const firebaseConfig = {
-    apiKey: String(import.meta.env.REACT_APP_API_KEY),
-    authDomain: String(import.meta.env.REACT_APP_AUTH_DOMAIN),
-    projectId: String(import.meta.env.REACT_APP_PROJECT_ID),
-    storageBucket: String(import.meta.env.REACT_APP_STORE_BUCKET),
-    messagingSenderId: String(import.meta.env.REACT_APP_MESSAGING_SENDER_ID),
-    appId: String(import.meta.env.REACT_APP_ID),
-    measurementId: String(import.meta.env.REACT_MEASUREMENT_ID),
+    apiKey: String(import.meta.env.VITE_APP_API_KEY),
+    authDomain: String(import.meta.env.VITE_APP_AUTH_DOMAIN),
+    projectId: String(import.meta.env.VITE_APP_PROJECT_ID),
+    storageBucket: String(import.meta.env.VITE_APP_STORE_BUCKET),
+    messagingSenderId: String(import.meta.env.VITE_APP_MESSAGING_SENDER_ID),
+    appId: String(import.meta.env.VITE_APP_ID),
+    measurementId: String(import.meta.env.VITE_MEASUREMENT_ID),
 };
 
 
