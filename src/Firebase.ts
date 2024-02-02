571a7f7c18e57bb4c040242538179001182dbfb1
bogbu <glutamine082442@gmail.com>
2024-02-02 23:38:05 +0900

    스토리지 세팅

--- src/Firebase.ts
+++ src/Firebase.ts
@@ -1,5 +1,6 @@
 import {initializeApp} from "firebase/app";
-import {getFirestore} from "firebase/firestore"
+import {getFirestore} from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
     apiKey: String(import.meta.env.VITE_APP_API_KEY),
     authDomain: String(import.meta.env.VITE_APP_AUTH_DOMAIN),
@@ -13,4 +14,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+export const storage = getStorage(app);
 export {db}
