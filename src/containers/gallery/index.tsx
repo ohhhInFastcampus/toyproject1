0c34781ccb733b175e6b20f9559b511d80ba4796
bogbu <glutamine082442@gmail.com>
2024-02-03 23:09:32 +0900

    페이지에서 getGalleryList 적용

--- src/containers/gallery/index.tsx
+++ src/containers/gallery/index.tsx
@@ -1,8 +1,16 @@
-import {mockCardList} from "@/mocks/galleryList.ts";
 import Gallery from "@/templates/gallery";
+import {getGalleryList} from "@/utils/gallery.ts";
+import {useEffect, useState} from "react";
+import {GalleryType} from "@/components/gallery/types.ts";
 const GalleryPage = () => {
+    const [galleryList, setGalleryList] = useState<GalleryType[]>([]);
+    useEffect(()=>{
+        getGalleryList().then((item)=>{
+            setGalleryList(item);
+        })
+    },[])
     return (
-        <Gallery list={mockCardList} />
+        <Gallery list={galleryList} />
     )
 }
 
