c7636b449c2b7cbf3be6afc8ff7cdec26b602ee1
bogbu <glutamine082442@gmail.com>
2024-02-04 00:48:09 +0900

    isUserLoggedIn 변수 임시로 지정한 부분 삭제

--- src/route/UseAuthenticate.ts
+++ src/route/UseAuthenticate.ts
@@ -3,7 +3,7 @@ import {getLocalStorage} from "@/utils/settingStorage.ts";
 export const useAuthenticate = (): boolean => {
     const userData = getLocalStorage("user");
     //TODO 로그인 기능 구현 후 isUserLoggedIn 기본값 false로 변경
-    let isUserLoggedIn = true;
+    let isUserLoggedIn = false;
     if (!(typeof userData === "string")) {
         isUserLoggedIn = true;
     }
