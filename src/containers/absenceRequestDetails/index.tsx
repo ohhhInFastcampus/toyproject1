86550fca60a6824368b0f56ceed1c2bf37abfa6d
bogbu <glutamine082442@gmail.com>
2024-02-04 01:38:26 +0900

    받아온 데이터 state에 세팅하는 로직 추가

--- src/containers/absenceRequestDetails/index.tsx
+++ src/containers/absenceRequestDetails/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import AbsenceRequestDetails from "@/templates/absenceRequestDetails";
 import {
   defaultValue,
@@ -6,7 +6,7 @@ import {
   selectItem,
   // tableData,
 } from "@/mocks/absenceDetail";
-import {getAbsenceRequestDetails} from "@/utils/absenceRequest.ts";
+import {getAbsenceRequestDetails, getAbsenceRequestDetailsFilter} from "@/utils/absenceRequest.ts";
 interface MockData {
   [key: string]: string[];
 }
@@ -15,16 +15,40 @@ export default function AbsenceRequestDetailsContainer() {
   // const [selectedValue, setSelectedValue] = useState<string>(defaultValue);
   const [absenceRequestList,setAbsenceRequestList] = useState<MockData>({})
   // selectedValue 상태를 업데이트하는 함수. 현재 이 함수는 AbsenceRequestDetails 컴포넌트로 전달되고 있음.
-  const getValue: React.Dispatch<React.SetStateAction<string>> = (value) => {
-    // setSelectedValue(value); // 이 부분을 주석 처리하여 ESLint 경고를 방지. 추후 필요시 주석 해제 예정.
-    console.log(value); // 함수가 호출되는 것을 확인하기 위한 임시 코드. 실제 사용시 제거.
-  };
+  const [value, getValue] = useState('');
+  useEffect(() => {
+    if(value !== "all") {
+      getAbsenceRequestDetailsFilter('type', value).then((list)=>{
+        let newList  : MockData = {};
+        list.map((item,index)=>{
+          let arr:MockData = {
+            [index] : [item.name, item.date, item.type, item.approver ]
+          }
+          newList = {...newList, ...arr};
+        })
+        setAbsenceRequestList(newList);
+        console.log(newList,"newList in ")
+      });
+    } else {
+      getAbsenceRequestDetails().then((list)=>{
+        let newList = absenceRequestList;
+        list.map((item,index)=>{
+          let arr:MockData = {
+            [index] : [item.name, item.date, item.type, item.approver ]
+          }
+          newList = {...newList, ...arr};
+        })
+        setAbsenceRequestList(newList);
+        console.log(newList,"newList")
+      })
+    }
+  }, [value]);
   useEffect(()=>{
     getAbsenceRequestDetails().then((list)=>{
       let newList = absenceRequestList;
       list.map((item,index)=>{
         let arr:MockData = {
-           [index] : [item.name, item.date, item.type, `${item.absenceTime}` ]
+           [index] : [item.name, item.date, item.type, item.approver ]
         }
         newList = {...newList, ...arr};
       })
