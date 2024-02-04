021ed553c0d5694929422b39ffdb3c9675133b48
bogbu <glutamine082442@gmail.com>
2024-02-04 19:15:21 +0900

    로그인 디테일 컴포넌트 코드 단순화

--- src/components/login/LoginDetail.tsx
+++ src/components/login/LoginDetail.tsx
@@ -1,27 +1,21 @@
-import React from 'react';
-import { Input } from "@/components/ui/input";
+import {Input} from "@/components/ui/input";
 import {LoginDetailType} from "@/components/login/types"
 
 
-function LoginDetail(props: LoginDetailType) {
-  const { id, type, placeholder, value, onChange } = props;
+function LoginDetail({id, type, placeholder, value, onChange}: LoginDetailType) {
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    onChange(e.target.value);
-  };
-
-  return (
-    <div className="np-1">
-      <Input  
-        className="njustify-items-center np-2 nw-60 nrounded-sm nborder-2 nborder-stone-300 np-1 nmt-1"
-        id={id}
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={handleInputChange}
-      />
-    </div>
-  );
+    return (
+        <div className="np-1">
+            <Input
+                className="njustify-items-center np-2 nw-60 nrounded-sm nborder-2 nborder-stone-300 np-1 nmt-1"
+                id={id}
+                type={type}
+                placeholder={placeholder}
+                value={value}
+                onChange={(e)=>{onChange(e.target.value)}}
+            />
+        </div>
+    );
 }
 
 export default LoginDetail;
