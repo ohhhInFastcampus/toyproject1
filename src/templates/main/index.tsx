e3620ab74fcf8ef7b3393cbb5cc5229e914c6d03
kano <ajvls98@gmail.com>
2024-02-05 18:31:10 +0900

    메인 중앙정렬

--- src/templates/main/index.tsx
+++ src/templates/main/index.tsx
@@ -3,21 +3,34 @@ import { Clock } from "@/components/main/Clock";
 import { Calendar } from "@/components/main/Calendar";
 import { TodoList } from "@/components/main/TodoList";
 import WorkingStatus from "@/components/main/WorkingStatus";
-import {MemberDetailType, MemberDetailTypes} from "@/components/main/types";
+import { MemberDetailType, MemberDetailTypes } from "@/components/main/types";
 import React from "react";
 
 interface MainPageType {
   user: MemberDetailType;
   working: MemberDetailTypes[];
-    handleSwitchChange :  (checked: boolean) => void
-    switchState : boolean,
-    settingWorkingStatus : ()=>void,
-    changeImage: (e: React.ChangeEvent<HTMLInputElement>) => Promise<string>
+  handleSwitchChange: (checked: boolean) => void;
+  switchState: boolean;
+  settingWorkingStatus: () => void;
+  changeImage: (e: React.ChangeEvent<HTMLInputElement>) => Promise<string>;
 }
-const Main = ({ user, working ,handleSwitchChange,switchState,settingWorkingStatus,changeImage}: MainPageType) => {
+const Main = ({
+  user,
+  working,
+  handleSwitchChange,
+  switchState,
+  settingWorkingStatus,
+  changeImage,
+}: MainPageType) => {
   return (
-    <div className="nflex ngap-[40px]">
-      <UserMain user={user} handleSwitchChange={handleSwitchChange} changeImage={changeImage} switchState={switchState}  settingWorkingStatus={settingWorkingStatus}/>
+    <div className="nflex ngap-[40px] nw-fit nm-auto np-10">
+      <UserMain
+        user={user}
+        handleSwitchChange={handleSwitchChange}
+        changeImage={changeImage}
+        switchState={switchState}
+        settingWorkingStatus={settingWorkingStatus}
+      />
       <div className="nw-[400px] nm-5 nflex nflex-col nitems-center ngap-y-10">
         <Clock />
         <Calendar />
