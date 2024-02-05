83792361c37c784400818db43b38937166c4bfec
Hong Joo Choi <hchoi2894@gmail.com>
2024-02-05 11:38:42 +0900

    프로필 이미지 클릭시 파일 변경하도록 수정

--- src/components/main/UserAvatar.tsx
+++ src/components/main/UserAvatar.tsx
@@ -1,21 +1,28 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { MemberDetailType } from "@/components/main/types";
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { Button } from "../ui/button";
 
-const UserAvatar = ({ profile,changeImage }: MemberDetailType) => {
-    const [img,setImg] = useState(profile);
-    const settingImage = (e: React.ChangeEvent<HTMLInputElement>) => {
-        changeImage(e).then((res)=>{
-            setImg(res);
-        })
-    }
+const UserAvatar = ({ profile, changeImage }: MemberDetailType) => {
+  const [img, setImg] = useState(profile);
+  const settingImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    changeImage(e).then((res) => {
+      setImg(res);
+    });
+  };
   return (
     <>
       <div className="nrelative ninline-block">
         <Avatar className="nflex njustify-center nw-40 nh-40 nbg-white">
-          <AvatarImage src={img} alt="@shadcn" />
+          <label className="cursor-pointer">
+            <input
+              type="file"
+              onChange={settingImage}
+              style={{ display: "none" }}
+            />
+            <AvatarImage src={img} alt="@shadcn" />
+          </label>
         </Avatar>
-        <input type="file" onChange={settingImage} />
       </div>
     </>
   );
