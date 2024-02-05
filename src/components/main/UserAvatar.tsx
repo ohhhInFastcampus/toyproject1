03b3121da54fcd57a8eb743f55989a980c9f7072
Hong Joo Choi <hchoi2894@gmail.com>
2024-02-05 14:03:06 +0900

    버튼 클릭시 프로필 이미지 수정할 수 있도록 버튼 추가

--- src/components/main/UserAvatar.tsx
+++ src/components/main/UserAvatar.tsx
@@ -1,10 +1,18 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { MemberDetailType } from "@/components/main/types";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Button } from "../ui/button";
 
 const UserAvatar = ({ profile, changeImage }: MemberDetailType) => {
   const [img, setImg] = useState(profile);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
   const settingImage = (e: React.ChangeEvent<HTMLInputElement>) => {
     changeImage(e).then((res) => {
       setImg(res);
@@ -14,15 +22,23 @@ const UserAvatar = ({ profile, changeImage }: MemberDetailType) => {
     <>
       <div className="nrelative ninline-block">
         <Avatar className="nflex njustify-center nw-40 nh-40 nbg-white">
-          <label className="cursor-pointer">
-            <input
-              type="file"
-              onChange={settingImage}
-              style={{ display: "none" }}
-            />
-            <AvatarImage src={img} alt="@shadcn" />
-          </label>
+          <AvatarImage src={img} alt="@shadcn" />
         </Avatar>
+        <label className="cursor-pointer">
+          <input
+            type="file"
+            ref={fileInputRef}
+            onChange={settingImage}
+            style={{ display: "none" }}
+          />
+          <Button
+            size="s"
+            className="nbg-gray-500 nabsolute nbottom-0 nright-3"
+            onClick={handleClick}
+          >
+            +
+          </Button>
+        </label>
       </div>
     </>
   );
