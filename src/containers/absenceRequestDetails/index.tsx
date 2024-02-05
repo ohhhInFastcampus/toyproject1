049783120ddf44ea4e1183b6793f4b1c937e4150
kano <ajvls98@gmail.com>
2024-02-05 16:08:10 +0900

    pagination추가

--- src/containers/absenceRequestDetails/index.tsx
+++ src/containers/absenceRequestDetails/index.tsx
@@ -10,6 +10,9 @@ import {
   getAbsenceRequestDetails,
   getAbsenceRequestDetailsFilter,
 } from "@/utils/absenceRequest.ts";
+import PaginationCreate from "@/components/PaginationCreate";
+import moment from "moment";
+
 interface MockData {
   name: string;
   date: string;
@@ -35,7 +38,7 @@ const processAbsenceRequestData = (response: MockData[]): ProcessedData => {
       ...acc,
       [index]: [
         item.name,
-        item.date,
+        moment(item.date).format("YYYY-MM-DD"),
         item.position,
         parsingAbsenceTimeToString(item.absenceTime),
         item.halfDayTime,
@@ -52,13 +55,25 @@ export default function AbsenceRequestDetailsContainer() {
   );
   // selectedValue 상태를 업데이트하는 함수. 현재 이 함수는 AbsenceRequestDetails 컴포넌트로 전달되고 있음.
   const [value, getValue] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const itemsPerPage = 15;
 
   const filterData = async (key: string, value: string) => {
     const response =
       key.length === 0
         ? await getAbsenceRequestDetails()
         : await getAbsenceRequestDetailsFilter(key, value);
-    const newList = processAbsenceRequestData(response);
+    // const newList = processAbsenceRequestData(response);
+
+    const totalItems = response.length;
+    setTotalPages(Math.ceil(totalItems / itemsPerPage));
+    const newList = processAbsenceRequestData(
+      response.slice(
+        (currentPage - 1) * itemsPerPage,
+        currentPage * itemsPerPage
+      )
+    );
     setAbsenceRequestList(newList);
   };
 
@@ -74,16 +89,27 @@ export default function AbsenceRequestDetailsContainer() {
     getAbsenceRequestDetails().then((list) => {
       const newList = processAbsenceRequestData(list);
       setAbsenceRequestList(newList);
+      console.log(list);
     });
   }, []);
+  useEffect(() => {
+    filterData("", "");
+  }, [currentPage]); // currentPage가 변경될 때마다 filterData 함수 호출
 
   return (
-    <AbsenceRequestDetails
-      headers={headers}
-      data={absenceRequestList}
-      defaultValue={defaultValue}
-      selectItem={selectItem}
-      getValue={getValue}
-    />
+    <>
+      <AbsenceRequestDetails
+        headers={headers}
+        data={absenceRequestList}
+        defaultValue={defaultValue}
+        selectItem={selectItem}
+        getValue={getValue}
+      />
+      <PaginationCreate
+        totalPages={totalPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+    </>
   );
 }
