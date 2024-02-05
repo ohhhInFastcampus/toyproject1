fe8fea9d2ad0d541a25aa3b0f797280a2eb0d124
bogbu <glutamine082442@gmail.com>
2024-02-05 14:12:24 +0900

    아바타 페이지에서 받는 props 수정

--- src/components/main/UserAvatar.tsx
+++ src/components/main/UserAvatar.tsx
@@ -1,46 +1,48 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { MemberDetailType } from "@/components/main/types";
-import React, { useState, useRef } from "react";
-import { Button } from "../ui/button";
-
-const UserAvatar = ({ profile, changeImage }: MemberDetailType) => {
-  const [img, setImg] = useState(profile);
-  const fileInputRef = useRef<HTMLInputElement>(null);
-
-  const handleClick = () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click();
+import React, {useRef, useState} from "react";
+import {Button} from "@/components/ui/button.tsx";
+interface userAvatarType {
+    profile : MemberDetailType,
+    changeImage :(e: React.ChangeEvent<HTMLInputElement>) => Promise<string>
+}
+const UserAvatar = ({ profile,changeImage }: userAvatarType) => {
+    const [img,setImg] = useState(profile.profile);
+    const settingImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+        changeImage(e).then((res)=>{
+            setImg(res);
+        })
     }
-  };
+    const fileInputRef = useRef<HTMLInputElement>(null);
 
-  const settingImage = (e: React.ChangeEvent<HTMLInputElement>) => {
-    changeImage(e).then((res) => {
-      setImg(res);
-    });
-  };
-  return (
-    <>
-      <div className="nrelative ninline-block">
-        <Avatar className="nflex njustify-center nw-40 nh-40 nbg-white">
-          <AvatarImage src={img} alt="@shadcn" />
-        </Avatar>
-        <label className="cursor-pointer">
-          <input
-            type="file"
-            ref={fileInputRef}
-            onChange={settingImage}
-            style={{ display: "none" }}
-          />
-          <Button
-            size="s"
-            className="nbg-gray-500 nabsolute nbottom-0 nright-3"
-            onClick={handleClick}
-          >
-            +
-          </Button>
-        </label>
-      </div>
-    </>
-  );
+    const handleClick = () => {
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
+    };
+    return (
+        <>
+            <div className="nrelative ninline-block">
+                <Avatar className="nflex njustify-center nw-40 nh-40 nbg-white">
+                    <AvatarImage src={img} alt="@shadcn" />
+                </Avatar>
+                <label className="cursor-pointer">
+                    <input
+                        type="file"
+                        ref={fileInputRef}
+                        onChange={settingImage}
+                        style={{ display: "none" }}
+                    />
+                    <Button
+                        size="s"
+                        className="nbg-gray-500 nabsolute nbottom-0 nright-3"
+                        onClick={handleClick}
+                    >
+                        +
+                    </Button>
+                </label>
+            </div>
+        </>
+    );
 };
 export { UserAvatar };
