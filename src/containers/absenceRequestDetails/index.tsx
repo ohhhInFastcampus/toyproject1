3fe7a88156c04836a3c28bcb6afedadffc719433
kano <ajvls98@gmail.com>
2024-02-05 11:32:28 +0900

    오전/오후 추가

--- src/containers/absenceRequestDetails/index.tsx
+++ src/containers/absenceRequestDetails/index.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import AbsenceRequestDetails from "@/templates/absenceRequestDetails";
 import {
   defaultValue,
@@ -6,65 +6,88 @@ import {
   selectItem,
   // tableData,
 } from "@/mocks/absenceDetail";
-import {getAbsenceRequestDetails, getAbsenceRequestDetailsFilter} from "@/utils/absenceRequest.ts";
+import {
+  getAbsenceRequestDetails,
+  getAbsenceRequestDetailsFilter,
+} from "@/utils/absenceRequest.ts";
 interface MockData {
   [key: string]: string[];
 }
-const parsingAbsenceTimeToString = (time :number) => {
-  if(time === 4) return "반차";
-  else return "연차"
-}
+const parsingAbsenceTimeToString = (time: number) => {
+  if (time === 4) return "반차";
+  else return "연차";
+};
 export default function AbsenceRequestDetailsContainer() {
-  // 상태를 선언하지만 현재는 사용되지 않음. 추후 사용 예정이므로 유지.
-  // const [selectedValue, setSelectedValue] = useState<string>(defaultValue);
-  const [absenceRequestList,setAbsenceRequestList] = useState<MockData>({})
+  const [absenceRequestList, setAbsenceRequestList] = useState<MockData>({});
   // selectedValue 상태를 업데이트하는 함수. 현재 이 함수는 AbsenceRequestDetails 컴포넌트로 전달되고 있음.
-  const [value, getValue] = useState('');
-  const filterData =  async (key: string, value : string) => {
-    if(key.length === 0) {
+  const [value, getValue] = useState("");
+  const filterData = async (key: string, value: string) => {
+    if (key.length === 0) {
       let newList = absenceRequestList;
       const response = await getAbsenceRequestDetails();
-      response.map((item,index)=>{
-        let arr:MockData = {
-          [index] : [item.name, item.date, item.position,parsingAbsenceTimeToString(item.absenceTime) , item.approver ]
-        }
-        newList = {...newList, ...arr};
-      })
+      console.log(response);
+      response.map((item, index) => {
+        let arr: MockData = {
+          [index]: [
+            item.name,
+            item.date,
+            item.position,
+            parsingAbsenceTimeToString(item.absenceTime),
+            item.halfDayTime,
+            item.approver,
+          ],
+        };
+        newList = { ...newList, ...arr };
+      });
       setAbsenceRequestList(newList);
-      console.log(newList,"newList")
+      console.log(newList, "newList");
     } else {
       const response = await getAbsenceRequestDetailsFilter(key, value);
-      let newList  : MockData = {};
-      response.map((item, index)=>{
-        let arr:MockData = {
-          [index] : [item.name, item.date, item.position,parsingAbsenceTimeToString(item.absenceTime) , item.approver ]
-        }
-        newList = {...newList, ...arr};
-      })
+      let newList: MockData = {};
+      response.map((item, index) => {
+        let arr: MockData = {
+          [index]: [
+            item.name,
+            item.date,
+            item.position,
+            parsingAbsenceTimeToString(item.absenceTime),
+            item.halfDayTime,
+            item.approver,
+          ],
+        };
+        newList = { ...newList, ...arr };
+      });
       setAbsenceRequestList(newList);
-      console.log(newList,"newList in ")
+      console.log(newList, "newList in ");
     }
-  }
+  };
   useEffect(() => {
-    if(value !== "all") {
-      filterData('absenceTime',value)
+    if (value !== "all") {
+      filterData("absenceTime", value);
     } else {
-      filterData('',value)
+      filterData("", value);
     }
   }, [value]);
-  useEffect(()=>{
-    getAbsenceRequestDetails().then((list)=>{
+  useEffect(() => {
+    getAbsenceRequestDetails().then((list) => {
       let newList = absenceRequestList;
-      list.map((item,index)=>{
-        let arr:MockData = {
-           [index] : [item.name, item.date, item.position,parsingAbsenceTimeToString(item.absenceTime) , item.approver ]
-        }
-        newList = {...newList, ...arr};
-      })
+      list.map((item, index) => {
+        let arr: MockData = {
+          [index]: [
+            item.name,
+            item.date,
+            item.position,
+            parsingAbsenceTimeToString(item.absenceTime),
+            item.halfDayTime,
+            item.approver,
+          ],
+        };
+        newList = { ...newList, ...arr };
+      });
       setAbsenceRequestList(newList);
-      console.log(newList,"newList")
+      console.log(newList, "newList");
     });
-  }, [])
+  }, []);
   return (
     <AbsenceRequestDetails
       headers={headers}
--- src/utils/absenceRequest.ts
+++ src/utils/absenceRequest.ts
@@ -1,44 +1,50 @@
-import {MemberDetailTypes} from "@/components/main/types.ts";
-import {getLocalStorage} from "@/utils/settingStorage.ts";
-import {collection, getDocs, addDoc, query, where} from "firebase/firestore";
-import {db} from "@/Firebase.ts";
-import {reqAbsenceType} from "@/templates/absenceRequest/types.ts";
+import { MemberDetailTypes } from "@/components/main/types.ts";
+import { getLocalStorage } from "@/utils/settingStorage.ts";
+import { collection, getDocs, addDoc, query, where } from "firebase/firestore";
+import { db } from "@/Firebase.ts";
+import { reqAbsenceType } from "@/templates/absenceRequest/types.ts";
 
 const user: MemberDetailTypes = getLocalStorage("user");
 export const postAbsenceRequest = async (request: reqAbsenceType) => {
-    const absenceRequest = collection(db, "absenceRequestDetails");
-    let setData: reqAbsenceType = {
-        name: request.name,
-        halfDayTime: request.halfDayTime,
-        approver: request.approver,
-        date: request.date,
-        position: request.position,
-        reason: request.reason,
-        absenceTime: request.absenceTime,
-        email: user.email
-    }
-    await addDoc(absenceRequest, setData);
-    return setData;
-}
+  const absenceRequest = collection(db, "absenceRequestDetails");
+  let setData: reqAbsenceType = {
+    name: request.name,
+    halfDayTime: request.halfDayTime,
+    approver: request.approver,
+    date: request.date,
+    position: request.position,
+    reason: request.reason,
+    absenceTime: request.absenceTime,
+    email: user.email,
+  };
+  await addDoc(absenceRequest, setData);
+  return setData;
+};
 export const getAbsenceRequestDetails = async (): Promise<reqAbsenceType[]> => {
-    let list: reqAbsenceType[] = [];
-    const absenceRequestCollection = collection(db, "absenceRequestDetails");
-    const absenceRequestList = await getDocs(absenceRequestCollection);
-    absenceRequestList.forEach((item) => {
-        const newItem = item.data() as reqAbsenceType;
-        newItem.email = user.email;
-        list.push(newItem);
-    });
-    return list;
-}
+  let list: reqAbsenceType[] = [];
+  const absenceRequestCollection = collection(db, "absenceRequestDetails");
+  const absenceRequestList = await getDocs(absenceRequestCollection);
+  absenceRequestList.forEach((item) => {
+    const newItem = item.data() as reqAbsenceType;
+    newItem.email = user.email;
+    list.push(newItem);
+  });
+  return list;
+};
 
-export const getAbsenceRequestDetailsFilter = async (key: string, filter: string): Promise<reqAbsenceType[]> => {
-    let list: reqAbsenceType[] = [];
-    const absenceRequestCollection = collection(db, "absenceRequestDetails");
-    const filtering = query(absenceRequestCollection, where(key, '==', key === "absenceTime" ? Number(filter) : filter))
-    const absenceRequestList = await getDocs(filtering);
-    absenceRequestList.forEach((item) => {
-        list.push(item.data() as reqAbsenceType);
-    });
-    return list;
-}
+export const getAbsenceRequestDetailsFilter = async (
+  key: string,
+  filter: string
+): Promise<reqAbsenceType[]> => {
+  let list: reqAbsenceType[] = [];
+  const absenceRequestCollection = collection(db, "absenceRequestDetails");
+  const filtering = query(
+    absenceRequestCollection,
+    where(key, "==", key === "absenceTime" ? Number(filter) : filter)
+  );
+  const absenceRequestList = await getDocs(filtering);
+  absenceRequestList.forEach((item) => {
+    list.push(item.data() as reqAbsenceType);
+  });
+  return list;
+};
